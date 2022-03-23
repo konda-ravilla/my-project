395da8700f5f226255dfb103275cc0f504eb958b
konda ravilla <kondaravilla000@gmail.com>
2022-03-23 16:03:31 +0530

    122 task completed

--- about.js
+++ about.js
@@ -1,9 +1,28 @@
 function about() {
    console.log('about')
 }
+<<<<<<< Updated upstream
 
 //132
 
 function task() {
    console.log('132 task completed')
 }
+=======
+//122 task
+let obj1 = {
+   color: ' red',
+   duplicate: function () {
+      // 
+      // 
+   }
+}
+
+// //132 task
+
+// function task() {
+//    console.log('task')
+//    //
+//    //
+// }
+>>>>>>> Stashed changes
