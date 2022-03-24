df498b7249645f537b26a74e7e23d76dbf21fa91
konda ravilla <kondaravilla000@gmail.com>
2022-03-24 11:55:52 +0530

    changes to master branches

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 let obj = {
-   name: 'latha',
-   age: 24,
-   move: function () {
-      console.log('move')
-   }
-}
+  name: 'latha',
+  age: 24,
+  move: function () {
+    console.log('move');
+  },
+  duplicate: function () {
+    console.log('duplicate');
+  },
+};
