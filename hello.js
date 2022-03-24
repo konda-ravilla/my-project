4291e62bc1e0e85f801b2aad4d965ae297b9cb5e
konda ravilla <kondaravilla000@gmail.com>
2022-03-24 11:52:17 +0530

    new change to develop branch

--- hello.js
+++ hello.js
@@ -1,7 +1,10 @@
 let obj = {
-   name: 'latha',
-   age: 24,
-   move: function () {
-      console.log('move')
-   }
-}
+  name: 'latha',
+  age: 24,
+  move: function () {
+    console.log('move');
+  },
+  play: function () {
+    console.log('play');
+  },
+};
